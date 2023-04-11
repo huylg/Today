02ad6f51a764625383b43884128e878b4c64b816
huylg <lygiahuy11111998.hcmus@gmail.com>
2023-04-11 22:29:59 +0700

    --wip-- [skip ci]

---
+++ Today/Models/Date+Today.swift
@@ -0,0 +1,25 @@
+//
+//  Date+Today.swift
+//  Today
+//
+//  Created by Ly Gia Huy on 11/04/2023.
+//
+
+import Foundation
+
+extension Date {
+    var dayAndTimeText: String {
+        let timeText = formatted(date: .omitted, time: .shortened)
+        
+        if Locale.current.calendar.isDateInToday(self){
+            let timeFormat = NSLocalizedString("Today at %", comment: "Today at time format string")
+            return String(format: timeFormat, timeText)
+        }else {
+            let dateFormat = formatted(.dateTime.month(.abbreviated).day())
+            let dateAndTimeFormat = NSLocalizedString("% at %", comment: "Date and time format string")
+            return String(format: dateAndTimeFormat, dateFormat, timeText)
+        }
+        
+    }
+    
+}
